775a63d70f8849742be3f736ce9dbbeaf763b4d4
ghadeer Alturaif <ghadeeralturaif@gmail.com>
2024-03-06 10:23:24 +0000

    done

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,7 @@ revenue,
 purchase_price,
 quantity,
 round(revenue - (cast(purchase_price as float64) * quantity),2) AS margin,
-round((cast(purchase_price as float64) * quantity) * quantity,2) As purchase_Cost
+round((cast(purchase_price as float64) * quantity) * quantity,2) As purchase_cost
 from {{ ref('stg_gz_raw_data__sales') }}
 left join {{ ref('stg_gz_raw_data__product') }}
 using(products_id)
