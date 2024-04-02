e06f55f1db778fc9fae1f8c0c27b6e804da2898c
ghadeer Alturaif <ghadeeralturaif@gmail.com>
2024-04-02 12:13:08 +0000

    done

---
+++ models/intermediate/int_campaign.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM {{ ref('stg_gz_raw_data__adwords') }}
+UNION ALL 
+SELECT *
+{{ ref('stg_gz_raw_data__bing') }}
+UNION ALL 
+SELECT *
+FROM {{ ref('stg_gz_raw_data__criteo') }}
+UNION ALL 
+SELECT *
+FROM {{ ref('stg_gz_raw_data__facebook') }}
+
