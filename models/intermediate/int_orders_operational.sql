51a8ab221be5db0af3f268709fe34917ecd5ec1e
ghadeer Alturaif <ghadeeralturaif@gmail.com>
2024-04-02 11:45:14 +0000

    done

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -4,3 +4,4 @@ margin + shipping_fee - logcost - cast(ship_cost as float64) AS Operational_marg
 from {{ ref('int_orders_margin') }}
 left join {{ ref('stg_gz_raw_data__ship') }}
 using(orders_id)
+ORDER BY orders_id desc
