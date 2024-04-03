b1bb1adfe624a2b0c02f7f1631aaf7eca28e2242
ghadeeralturaif@gmail.com <ghadeeralturaif@gmail.com>
2024-04-03 11:05:12 +0000

    k

--- models/intermediate/test.sql
+++ models/intermediate/test.sql
@@ -1,4 +1,12 @@
-select *
-from {{ ref('stg_gz_raw_data__adwords') }}
-inner join {{ ref('stg_gz_raw_data__bing') }} 
-using(campaign_key)
+select 
+products_id,
+orders_id,
+date_date,
+revenue,
+purchase_price,
+quantity,
+round(revenue - (cast(purchase_price as float64) * quantity),2) AS margin,
+round((cast(purchase_price as float64) * quantity) * quantity,2) As purchase_cost
+from {{ ref('stg_gz_raw_data__sales') }}
+left join {{ ref('stg_gz_raw_data__product') }}
+using(products_id)
