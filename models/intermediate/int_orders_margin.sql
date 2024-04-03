8d4a7cc09419784c61eacc5167d69239f1c9f239
ghadeeralturaif@gmail.com <ghadeeralturaif@gmail.com>
2024-04-03 11:19:10 +0000

    jj

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -5,6 +5,6 @@ round(SUM(revenue),2) as revenue,
 round(SUM(quantity),2) as quantity,
 round((cast(purchase_price as float64) * quantity) * quantity,2) As purchase_Cost,
 round(revenue - (cast(purchase_price as float64) * quantity),2) AS margin
-from {{ref("int_sales_margin")}}
+from `my-project-week2-414708`.`dbt_ghadeer`.`int_sales_margin`
 GROUP BY orders_id,date_date
-ORDER BY orders_id DESC
+ORDER BY orders_id DESC;
